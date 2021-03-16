6524d363a6e0e8144db438e9dcc9e4209207c71f
Miles Crighton <miles.crighton@gmail.com>
2021-03-16 17:22:02 +0000

    feat(web): Copy alert

--- src/clipboard.js
+++ src/clipboard.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", setupClipboardEvents);
 
 function copyColorToClipboard(e) {
+    const currentTarget = e.currentTarget;
     const color = e.currentTarget.getAttribute("data-color");
 
     navigator.clipboard.writeText(color).then(
         () => {
             /* clipboard successfully set */
+            const alertNode = document.createElement("div");
+            const textNode = document.createTextNode(`COPIED ${color}`);
+            alertNode.appendChild(textNode);
+            alertNode.setAttribute("aria-live", "polite");
+            alertNode.setAttribute("role", "alert");
+            alertNode.className = "color-button__alert";
+            currentTarget.appendChild(alertNode);
+            setTimeout(() => currentTarget.removeChild(alertNode), 2000);
         },
         () => {
             /* clipboard write failed */
